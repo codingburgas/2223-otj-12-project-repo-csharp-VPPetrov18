aeb7c74aa777cd776cdada56399f9c84554d13c9
VPPetrov18 <VPPetrov18@codingburgas.bg>
2023-02-20 12:54:19 +0200

    start of role authorization

--- MainApplication/MainApplication/Program.cs
+++ MainApplication/MainApplication/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("TrenGCnn")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>().AddDefaultTokenProviders()
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
