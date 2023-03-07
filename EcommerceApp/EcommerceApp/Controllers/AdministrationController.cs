c591fe590fd413634db61ded20accb74a9f1d9a9
VPPetrov18 <VPPetrov18@codingburgas.bg>
2023-03-07 10:09:37 +0200

    Edit user as Admin

--- EcommerceApp/EcommerceApp/Controllers/AdministrationController.cs
+++ EcommerceApp/EcommerceApp/Controllers/AdministrationController.cs
@@ -3,6 +3,7 @@ using EcommerceApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.ConstrainedExecution;
 
 namespace EcommerceApp.Controllers
 {
@@ -24,6 +25,67 @@ namespace EcommerceApp.Controllers
         {
             var users = userManager.Users;
             return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var userClaims = await userManager.GetClaimsAsync(user);
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Roles = userRoles
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            var user = await userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+
+                var result = await userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
         }
 
         [HttpGet]
---
+++ EcommerceApp/EcommerceApp/Models/EditUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApp.Models
+{
+    public class EditUserViewModel
+    {
+        public EditUserViewModel()
+        {
+            Roles = new List<string>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public IList<string> Roles { get; set; }
+    }
+}
