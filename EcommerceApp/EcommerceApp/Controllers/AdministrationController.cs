f2f79a077543eb2ae6af44530c7b50f8625e4c64
VPPetrov18 <VPPetrov18@codingburgas.bg>
2023-03-09 11:02:17 +0200

    *Delete confirmation -Users -Roles

--- EcommerceApp/EcommerceApp/Controllers/AdministrationController.cs
+++ EcommerceApp/EcommerceApp/Controllers/AdministrationController.cs
@@ -48,6 +48,37 @@ namespace EcommerceApp.Controllers
             }
         }
 
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListRoles");
+            }
+        }
+
+
+
         [HttpGet]
         public IActionResult ListUsers()
         {
